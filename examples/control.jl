9f7cbb085b43b5cb63bbb9cb55b031de94686fa1
Daniel Larby <dan_larby@hotmail.co.uk>
2023-05-18 18:22:00 +0100

    Example of 1kHz control loop running over julia. Still experiencing problems with timeouts (in C++ and in Julia), needs work.

--- examples/control.jl
+++ examples/control.jl
@@ -1,8 +1,12 @@
+using Revise
+using RobotDynamics
 using StaticArrays
+using GLMakie
 
-inpipe = open("cxxjuliapipe", "r")
-outpipe = open("juliacxxpipe", "w")
-println("Opened pipes")
+struct Pipes
+    inpipe::IOStream
+    outpipe::IOStream
+end
 
 function parse_robot_state(str::AbstractString)
     float_strs = split(str, ',')
@@ -12,25 +16,81 @@ function parse_robot_state(str::AbstractString)
     SVector{14, Float64}(floats)
 end
 
-function parse_robot_state()
-    line = readline(inpipe)
-    parse_robot_state(line)
+function parse_robot_state(inpipe::IOStream; timeout)
+    start = time()
+    while true
+        line = readline(inpipe)
+        if (line != "")
+            return parse_robot_state(line)
+        end
+        if (time() - start) > timeout
+            throw(ErrorException("Timeout >$(timeout) seconds reading torques. Last read line was $(line)"))
+        end
+    end
 end
+parse_robot_state(p::Pipes, args...;kwargs...) = parse_robot_state(p.inpipe, args...; kwargs...)
 
-function write_robot_torques(torques::SVector{7, Float64})
+function write_robot_torques(outpipe::IOStream, torques::SVector{7, Float64})
     write(outpipe, join(torques, ","))
     write(outpipe, "\n")
     flush(outpipe)
 end
+write_robot_torques(p::Pipes, args...) = write_robot_torques(p.outpipe, args...)
 
 function test()
-    write_robot_torques(SVector{7}(0.,1.,0.,1.,0.,1.,0.))
-    println(parse_robot_state())
+    # Prepare fig
+    # fig = Makie.Figure()
+    # ax = Axis3(fig[1, 1]; aspect = :data, limits = (-1.0, 1.0, -1.0, 1.0, 0.0, 1.0))
+    # display(fig)
+    
+    # # Load and plot mechanism
+    # mechanism = RobotDynamics.RSON.parseRSON("/home/daniellarby/RSONs/rsons/franka_panda/pandaDummyInstrument.rson")
+    # m = compile(mechanism)
+    # q_plot = Observable(@SVector(zeros(7)))
+    q_idxs = @SVector([i for i in 1:7])
+    # robotplot!(ax, m, q_plot, visuals(mechanism))
 
+    # Open communication
+    pipes = Pipes(open("cxxjuliapipe", "r"), open("juliacxxpipe", "w"))
+    write_robot_torques(pipes, SVector{7}(0.,0.,0.,0.,0.,0.,0.))
+    q_init = parse_robot_state(pipes; timeout=10.0)[q_idxs]
+    # println("q_init: $(q_init)")
+    # q_plot[] = q_init
+    # println("Set q_init on plot...")
+    # sleep(.0001)
+    
+    function plot_func(q)
+        # println("Plotting...")
+        # q_plot[] = q
+        println(q)
+        # sleep(.00001)
+    end
+
+    function loop_func(i, q_prev)
+        # start = time()
+        write_robot_torques(pipes, SVector{7}(0.,0.,0.,0.,0.,0.,0.))
+        q = parse_robot_state(pipes; timeout=2e-3)[q_idxs]
+        if ( (i % 100) == 0)
+            plot_func(q)
+        end
+        q
+    end
+
+
+    loop_func(0, q_init)
+    println("Loop time:     (should be less than 1ms)")
+    @time(loop_func(1, q_init))
+    println("Loop time with plot:     (should be less than 1ms)")
+    @time(loop_func(0, q_init))
+    
+    println("Ready...")
+    
+    i = 0
+    q = q_init
     while true
-        println(parse_robot_state())
+        i += 1
+        q = loop_func(i, q)
     end
 end
 
 test()
-
--- examples/julia.cpp
+++ examples/julia.cpp
@@ -45,7 +45,7 @@ void pipe_state(std::ofstream &f, franka::RobotState robot_state)
     f << msg << std::endl;
 }
 
-void parse_comma_separated_doubles(std::vector<double> &vect, std::stringstream& ss)
+void parse_comma_separated_doubles(std::vector<double> &vect, std::stringstream &ss)
 {
     while (ss.good())
     {
@@ -55,20 +55,21 @@ void parse_comma_separated_doubles(std::vector<double> &vect, std::stringstream&
     }
 }
 
-Eigen::VectorXd parse_torques(std::ifstream& infifo) {
-        std::string line;
-        std::getline(infifo, line);
-        std::vector<double> torque_data;
-        std::stringstream ss(line);
-        
-        parse_comma_separated_doubles(torque_data, ss);
+Eigen::VectorXd parse_torques(std::ifstream &infifo)
+{
+    std::string line;
+    std::getline(infifo, line);
+    std::vector<double> torque_data;
+    std::stringstream ss(line);
 
-        std::cout << torque_data.size() << std::endl;
-        Eigen::VectorXd torques = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(torque_data.data(), torque_data.size());
-        if (torques.size() != 7) {
-            throw "Parsed torques vector not length 7!";
-        }
-        return torques;
+    parse_comma_separated_doubles(torque_data, ss);
+
+    Eigen::VectorXd torques = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(torque_data.data(), torque_data.size());
+    if (torques.size() != 7)
+    {
+        throw "Parsed torques vector not length 7!";
+    }
+    return torques;
 }
 
 int main(int argc, char **argv)
@@ -101,79 +102,62 @@ int main(int argc, char **argv)
         franka::Model model = robot.loadModel();
         franka::RobotState initial_state = robot.readOnce();
 
+        // Repeat for warmup..
         pipe_state(outfifo, initial_state);
         auto torques = parse_torques(infifo);
+        pipe_state(outfifo, initial_state);
+        torques = parse_torques(infifo);
+        pipe_state(outfifo, initial_state);
+        torques = parse_torques(infifo);
+        pipe_state(outfifo, initial_state);
+        torques = parse_torques(infifo);
 
-        while (true) {
-            std::cout << "Writing..." << std::endl;
-            pipe_state(outfifo, initial_state);
-            sleep(1);
-        }
 
-        // std::string line;
-        // std::getline(infifo, line);
-        // std::vector<double> torque_data;
+        // while (true)
+        // {
+        //     std::cout << "Writing..." << std::endl;
+        //     initial_state = robot.readOnce();
+        //     pipe_state(outfifo, initial_state);
+        //     sleep(0.02);
+        // }
 
-        // printf("Line to parse\n");
-        // std::cout << line << std::endl;
-        // std::stringstream ss(line);
-        // parse_comma_separated_doubles(torque_data, ss);
+        // define callback for the torque control loop
+        std::function<franka::Torques(const franka::RobotState &, franka::Duration)>
+            impedance_control_callback = [&](const franka::RobotState &robot_state,
+                                             franka::Duration duration) -> franka::Torques
+        {
+            // Tell julia the state
+            pipe_state(outfifo, robot_state);
+            // Read from julia the control
+            auto tau_d = parse_torques(infifo);
 
-        // std::cout << torque_data.size() << std::endl;
-        // Eigen::VectorXd torques = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(torque_data.data(), torque_data.size());
+            // convert to double array
+            std::array<double, 7> tau_d_array{};
+            Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
+            return tau_d_array;
+        };
 
-        //     // Register point
-        //     Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
-        //     const Eigen::Vector3d ee_offset({0.377, 0.0, 0.042});
-        //     const auto frame = franka::Frame::kEndEffector;
-        //     Eigen::Vector3d starting_pos(initial_transform * ee_offset);
-
-        //     WorldCoord ee;
-        //     ee.offset = ee_offset;
-        //     ee.ref = starting_pos;
-
-        //     // define callback for the torque control loop
-        //     std::function<franka::Torques(const franka::RobotState &, franka::Duration)>
-        //         impedance_control_callback = [&](const franka::RobotState &robot_state,
-        //                                          franka::Duration duration) -> franka::Torques
-        //     {
-        //         ImpedanceCoordArgs iargs(robot_state, model, frame);
-        //         auto ee_coord = computeWorldCoord(iargs, ee);
-        //         if (ee_coord.z.norm() > 0.15)
-        //         {
-        //             throw std::runtime_error("Aborting; too far away from starting pose!");
-        //         }
-
-        //         // compute control
-        //         Eigen::VectorXd tau_d(7);
-        //         tau_d.fill(0);
-
-        //         // convert to double array
-        //         std::array<double, 7> tau_d_array{};$
-        //         Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
-        //         return tau_d_array;
-        //     };
-
-        //     // start real-time control loop
-        //     std::cout << "WARNING: Collision thresholds are set to high values. "
-        //               << "Make sure you have the user stop at hand!" << std::endl
-        //               << "After starting try to push the robot and see how it reacts." << std::endl
-        //               << "Press Enter to continue..." << std::endl;
-        //     std::cin.ignore();
-        //     robot.control(impedance_control_callback);
+        // start real-time control loop
+        std::cout << "WARNING: Collision thresholds are set to high values. "
+                  << "Make sure you have the user stop at hand!" << std::endl
+                  << "After starting try to push the robot and see how it reacts." << std::endl
+                  << "Press Enter to continue..." << std::endl;
+        std::cin.ignore();
+        robot.control(impedance_control_callback);
     }
     catch (const franka::Exception &ex)
     {
-        // print exception
         std::cout << ex.what() << std::endl;
     }
     catch (const std::exception &ex)
     {
-        std::cerr << ex.what() << std::endl;
+        std::cout << ex.what() << std::endl;
     }
 
     outfifo.close();
+    infifo.close();
     unlink(outfifoname);
+    unlink(infifoname);
 
     return 0;
 }
